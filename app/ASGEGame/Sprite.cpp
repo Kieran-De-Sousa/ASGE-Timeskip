06abbed2d1a65901455d9b0d7fc2edfac4ff31af
Kieran De Sousa <kieran2.desousa@live.uwe.ac.uk>
2022-02-26 15:12:19 +0000

    Add: Base sprite class
    - Added base sprite class
      - Initialisation
      - Position setting

---
+++ app/ASGEGame/Sprite.cpp
@@ -0,0 +1,39 @@
+#include "Sprite.h"
+
+Sprite::Sprite(ASGE::Renderer& rendererRef) : renderer(&rendererRef) {}
+Sprite::~Sprite() {}
+
+bool Sprite::initialiseSprite(std::string& filename)
+{
+  sprite = renderer->createUniqueSprite();
+  if (!sprite->loadTexture(filename))
+  {
+    Logging::WARN("Could not load file: " + filename);
+    return false;
+  }
+  /**
+   * @details Sets default values for sprite initialisation
+   */
+  setPosition(defaultSpritePosition[0], defaultSpritePosition[1]);
+  setSpriteVariables(defaultSpriteVariables[0], defaultSpriteVariables[1], defaultZOrder);
+  sprite->scale(1);
+
+  return true;
+}
+ASGE::Sprite* Sprite::getSprite()
+{
+  return sprite.get();
+}
+
+void Sprite::setPosition(float x, float y)
+{
+  sprite->xPos(x);
+  sprite->yPos(y);
+}
+
+void Sprite::setSpriteVariables(float width, float height, int16_t z_order)
+{
+  sprite->width(width);
+  sprite->height(height);
+  sprite->setGlobalZOrder(z_order);
+}
---
+++ app/ASGEGame/Sprite.h
@@ -0,0 +1,49 @@
+#ifndef ASGEGAME_SPRITE_H
+#define ASGEGAME_SPRITE_H
+
+#include <Engine/Logger.hpp>
+#include <Engine/OGLGame.hpp>
+#include <Engine/Sprite.hpp>
+
+/**
+ * @brief Base sprite class that is inherited by any object requiring ASGE Sprites
+ */
+class Sprite
+{
+ public:
+  /**
+   * @details Renderer required to be passed into constructor for creation of any Sprite related
+   * objects
+   * @param rendererRef
+   */
+  explicit Sprite(ASGE::Renderer& rendererRef);
+  virtual ~Sprite();
+
+  /**
+   * @details Pass filename in std::string to initialise a ASGE sprite with default values
+   * @see setPosition
+   * @param filename
+   * @return
+   */
+  bool initialiseSprite(std::string& filename);
+  ASGE::Sprite* getSprite();
+
+  /**
+   * @brief MAYBE NOT NEEDED DUE TO ASGE SPRITES HAVING xPos() AND yPos()
+   * @param x
+   * @param y
+   */
+  void setPosition(float x, float y);
+  void setSpriteVariables(float width, float height, int16_t z_order);
+
+ private:
+  std::unique_ptr<ASGE::Sprite> sprite;
+  std::unique_ptr<ASGE::Renderer> renderer;
+
+  /// Would be const due to not needing to be changed, however this results in compiler errors.
+  std::array<float, 2> defaultSpritePosition{ 0, 0 };
+  std::array<float, 2> defaultSpriteVariables{ 16, 16 };
+  int16_t defaultZOrder = 0;
+};
+
+#endif // ASGEGAME_SPRITE_H

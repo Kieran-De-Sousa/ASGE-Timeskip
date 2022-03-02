84e892cd8b0a89110a0270a3644fe7d32ec6fff3
Moosish <72136932+Moosish@users.noreply.github.com>
2022-03-02 13:41:05 +0000

    1

--- app/ASGEGame/ASGEGame.cpp
+++ app/ASGEGame/ASGEGame.cpp
@@ -14,33 +14,34 @@ ASGENetGame::ASGENetGame(const ASGE::GameSettings& settings) :
   key_callback_id     = inputs->addCallbackFnc(ASGE::E_KEY, &ASGENetGame::keyHandler, this);
   inputs->use_threads = false;
 
-  ship = renderer->createUniqueSprite();
-  ship->loadTexture("/data/sprites/player_ship.png");
-
-  camera_one_label.setFont(*game_font).setString("Camera 1").setPosition({ 0, 55 }).setScale(1.5);
-  camera_two_label.setFont(*game_font).setString("Camera 2").setPosition({ 960, 55 }).setScale(1.5);
+  //  ship = renderer->createUniqueSprite();
+  //  ship->loadTexture("/data/sprites/player_ship.png");
+  //
+  //  camera_one_label.setFont(*game_font).setString("Camera 1").setPosition({ 0, 55
+  //  }).setScale(1.5); camera_two_label.setFont(*game_font).setString("Camera 2").setPosition({
+  //  960, 55 }).setScale(1.5);
 
   initAudio();
 
   /// TESTING FOR DEFAULT CONSTRUCTORS OF DIFFERENT SPRITE OBJECT CLASSES
   /// @note DELETE OR REMOVE ME WHEN TESTING IS COMPLETE
-  testSprite = std::make_unique<Sprite>(*renderer);
-  testEntity = std::make_unique<Entity>(*renderer);
-  testPlayer = std::make_unique<Player>(*renderer);
+  //  testSprite = std::make_unique<Sprite>(*renderer);
+  //  testEntity = std::make_unique<Entity>(*renderer);
+  //  testPlayer = std::make_unique<Player>(*renderer);
 }
 
 void ASGENetGame::initAudio()
 {
-  audio_engine.init();
-  ASGE::FILEIO::File bg_audio_file;
-  if (bg_audio_file.open("/data/audio/cyberpunk.mp3"))
-  {
-    auto buffer = bg_audio_file.read();
-    auto length = static_cast<unsigned int>(buffer.length);
-    background_audio.loadMem(buffer.as_unsigned_char(), length, false, false);
-    background_audio.setLooping(true);
-    audio_engine.play(background_audio);
-  }
+  //  audio_engine.init();
+  //  ASGE::FILEIO::File bg_audio_file;
+  //  if (bg_audio_file.open("/data/audio/cyberpunk.mp3"))
+  //  {
+  //    auto buffer = bg_audio_file.read();
+  //    auto length = static_cast<unsigned int>(buffer.length);
+  //    background_audio.loadMem(buffer.as_unsigned_char(), length, false, false);
+  //    background_audio.setLooping(true);
+  //    audio_engine.play(background_audio);
+  //  }
 }
 
 /**
@@ -95,26 +96,26 @@ void ASGENetGame::fixedUpdate(const ASGE::GameTime& us)
  */
 void ASGENetGame::update(const ASGE::GameTime& us)
 {
-  // process single gamepad
-  if (auto gamepad = inputs->getGamePad(); gamepad.is_connected)
-  {
-    velocity.x = gamepad.axis[ASGE::GAMEPAD::AXIS_LEFT_X] * 500;
-    velocity.y = gamepad.axis[ASGE::GAMEPAD::AXIS_LEFT_Y] * 500;
-
-    if (gamepad_state.contains(gamepad.idx))
-    {
-      if (
-        ((gamepad.buttons[ASGE::GAMEPAD::BUTTON_X]) == 0U) &&
-        ((gamepad_state.at(gamepad.idx).buttons[ASGE::GAMEPAD::BUTTON_X]) != 0U))
-      {
-        Logging::DEBUG("X PRESSED");
-        velocity.y *= 10;
-      }
-    }
-  }
-
-  ship->xPos(static_cast<float>(ship->xPos() + velocity.x * us.deltaInSecs()));
-  ship->yPos(static_cast<float>(ship->yPos() + velocity.y * us.deltaInSecs()));
+  //  // process single gamepad
+  //  if (auto gamepad = inputs->getGamePad(); gamepad.is_connected)
+  //  {
+  //    velocity.x = gamepad.axis[ASGE::GAMEPAD::AXIS_LEFT_X] * 500;
+  //    velocity.y = gamepad.axis[ASGE::GAMEPAD::AXIS_LEFT_Y] * 500;
+  //
+  //    if (gamepad_state.contains(gamepad.idx))
+  //    {
+  //      if (
+  //        ((gamepad.buttons[ASGE::GAMEPAD::BUTTON_X]) == 0U) &&
+  //        ((gamepad_state.at(gamepad.idx).buttons[ASGE::GAMEPAD::BUTTON_X]) != 0U))
+  //      {
+  //        Logging::DEBUG("X PRESSED");
+  //        velocity.y *= 10;
+  //      }
+  //    }
+  //  }
+  //
+  //  ship->xPos(static_cast<float>(ship->xPos() + velocity.x * us.deltaInSecs()));
+  //  ship->yPos(static_cast<float>(ship->yPos() + velocity.y * us.deltaInSecs()));
 
   // retrieve all connected gamepads and store their states
   for (auto& gamepad : inputs->getGamePads())
@@ -129,18 +130,18 @@ void ASGENetGame::update(const ASGE::GameTime& us)
  */
 void ASGENetGame::render(const ASGE::GameTime& /*us*/)
 {
-  // example of split screen. just remove viewports and use
-  // a single camera if you don't require the use of split screen
-  renderer->setViewport(ASGE::Viewport{ 0, 0, 960, 1080 });
-  renderer->setProjectionMatrix(camera_one.getView());
-  renderer->render(*ship);
-  renderer->setViewport(ASGE::Viewport{ 960, 0, 960, 1080 });
-  renderer->setProjectionMatrix(camera_two.getView());
-  renderer->render(*ship);
-
-  // reset the view and don't use a camera, useful for HUD
-  renderer->setViewport(ASGE::Viewport{ 0, 0, 1920, 1080 });
-  renderer->setProjectionMatrix(0, 0, 1920, 1080);
-  renderer->render(camera_one_label);
-  renderer->render(camera_two_label);
+  //  // example of split screen. just remove viewports and use
+  //  // a single camera if you don't require the use of split screen
+  //  renderer->setViewport(ASGE::Viewport{ 0, 0, 960, 1080 });
+  //  renderer->setProjectionMatrix(camera_one.getView());
+  //  renderer->render(*ship);
+  //  renderer->setViewport(ASGE::Viewport{ 960, 0, 960, 1080 });
+  //  renderer->setProjectionMatrix(camera_two.getView());
+  //  renderer->render(*ship);
+  //
+  //  // reset the view and don't use a camera, useful for HUD
+  //  renderer->setViewport(ASGE::Viewport{ 0, 0, 1920, 1080 });
+  //  renderer->setProjectionMatrix(0, 0, 1920, 1080);
+  //  renderer->render(camera_one_label);
+  //  renderer->render(camera_two_label);
 }

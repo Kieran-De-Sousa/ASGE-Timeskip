afe76d098c9caf02d03020e432ee7b6c514f0568
Joey <joeybutcher1000@gmail.com>
2022-04-27 23:27:46 +0100

    UI - Kill Count

--- app/ASGEGame/Sprites/PlayerUI.h
+++ app/ASGEGame/Sprites/PlayerUI.h
@@ -13,6 +13,7 @@ class PlayerUI : public GameComponent
   virtual ~PlayerUI() override = default;
 
   void init();
+  void setLocations(const ASGE::Point2D& player1, const ASGE::Point2D& player2);
 
   // sprite rendering functions
   // health functions
@@ -31,6 +32,18 @@ class PlayerUI : public GameComponent
   ASGE::Sprite* getP1ActiveWep();
   ASGE::Sprite* getP2ActiveWep();
 
+  // kill count functions
+  ASGE::Sprite* getP1KillsIndicator();
+  ASGE::Sprite* getP2KillsIndicator();
+  ASGE::Sprite* getP1KillCount();
+  ASGE::Sprite* getP1KillCountExtra();
+  ASGE::Sprite* getP2KillCount();
+  ASGE::Sprite* getP2KillCountExtra();
+
+  // kill count numbers
+  int getP1KillCountVal();
+  int getP2KillCountVal();
+
   // health stuff
   void removeHealth(int playerID);
   void addHealth(int playerID);
@@ -46,6 +59,8 @@ class PlayerUI : public GameComponent
   // random vars
   int p1_health_val = 3;
   int p2_health_val = 3;
+  int p1_kills_val  = 0;
+  int p2_kills_val  = 0;
 
   // weapon stuff: 1 = Machine Gun, 2 = Shotgun
   int p1_wep_choice = 1;
@@ -64,6 +79,13 @@ class PlayerUI : public GameComponent
   std::unique_ptr<Sprite> p2_wep_indicator;
   std::unique_ptr<Sprite> p1_active_wep;
   std::unique_ptr<Sprite> p2_active_wep;
+
+  std::unique_ptr<Sprite> p1_kills_indicator;
+  std::unique_ptr<Sprite> p2_kills_indicator;
+  std::unique_ptr<Sprite> p1_kill_count;
+  std::unique_ptr<Sprite> p2_kill_count;
+  std::unique_ptr<Sprite> extra_p1_kill_count;
+  std::unique_ptr<Sprite> extra_p2_kill_count;
 
   // renderer
   ASGE::Renderer* renderer;
